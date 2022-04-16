f232764564650fc7b7aec61320f0d7f8dd6c80b4
dmbappi.bd@gmail.com <dmbappi.bd@gmail.com>
2022-04-16 12:43:34 +0600

    cart summary added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -11,11 +11,19 @@ const Cart = (props) => {
     // for(const product of cart){
     //     total = total + product.price;
     // }
+
+    const shipping = 15;
+    const tax = (total + shipping) * 10;
+    const grandTotal = total + shipping + tax;
+
     return (
         <div>
             <h3> Order Summary</h3>
             <h5>Items Ordered: {props.cart.length}</h5>
-            <p>Total: {total}</p>
+            <p>Total: {total.toFixed(2)}</p>
+            <p>Shipping: {shipping}</p>
+            <p>tax: {tax.toFixed(2)}</p>
+            <p>Grand total: {grandTotal.toFixed(2)}</p>
         </div>
     );
 };
