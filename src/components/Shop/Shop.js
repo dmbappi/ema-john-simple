e2c06d54e234b56f2e05b08d442093031f586110
dmbappi.bd@gmail.com <dmbappi.bd@gmail.com>
2022-04-16 11:56:08 +0600

    cart state setup and update cart and total on click

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,24 +1,36 @@
 import React, { useEffect, useState } from 'react';
+import Cart from '../Cart/Cart';
+import Product from '../Product/Product';
 import './Shop.css'
 
 const Shop = () => {
     const [products,setProducts] = useState([]);
+    const [cart,setCart] = useState([]);
     useEffect(()=>{
         fetch('./products.JSON')
         .then(res => res.json())
         .then(data => setProducts(data))
     },[])
+
+    const handleAddToCart = (product) => {
+        // console.log(product.name);
+        const newCart = [...cart,product];
+        setCart(newCart);
+    }
     return (
         <div className='shop-container'>
             <div className="product-container">
                 <h1>products:{products.length} </h1>
                 {
-                    products.map(product=> console.log(product))
+                    products.map(product=> <Product 
+                        key={product.key}
+                        product={product}
+                        handleAddToCart={handleAddToCart}
+                          />)
                 }
             </div>
             <div className="cart-container">
-                <h3> Order Summary</h3>
-                <h5>Items Ordered: </h5>
+                <Cart cart={cart}/>
             </div>
         </div>
     );
