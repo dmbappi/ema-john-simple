7d84f6bab06e48dd79b5f97322628cb199f9b518
dmbappi.bd@gmail.com <dmbappi.bd@gmail.com>
2022-04-16 12:34:19 +0600

    cart.reducer added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,10 +3,14 @@ import './Cart.css';
 
 const Cart = (props) => {
     const {cart} = props;
-    let total = 0;
-    for(const product of cart){
-        total = total + product.price;
-    }
+    const totalReducer = (previous,product) => previous + product.price;
+    const total = cart.reduce(totalReducer,0);
+
+    // const total = cart.reduce((previous,product) => previous + product.price,0);
+    // let total = 0;
+    // for(const product of cart){
+    //     total = total + product.price;
+    // }
     return (
         <div>
             <h3> Order Summary</h3>
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee,faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+
 import './Product.css';
 const Product = (props) => {
     // console.log(props.product);
     const {name,img,seller,price, stock}=props.product;
+    const cartIcon = <FontAwesomeIcon icon={faShoppingCart}/>
     return (
         <div className='product'>
             <div>
@@ -13,7 +18,7 @@ const Product = (props) => {
             <p><small>by:{seller}</small></p>
             <p>Price: {price}</p>
             <p><small>only {stock} left in stock - order soon</small></p>
-            <button onClick={() => props.handleAddToCart(props.product) } className="btn-regular">add to cart</button>
+            <button onClick={() => props.handleAddToCart(props.product) } className="btn-regular">{cartIcon}add to cart</button>
             
             </div>
 
